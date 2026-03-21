7bd205808ea6633197ee9997ea858445e9d2e556
viktor-taraba <victortaraba@gmail.com>
2026-03-21 18:47:41 +0200

    add SQLite database for conversation memory

--- homework-lesson-4/agent.py
+++ homework-lesson-4/agent.py
@@ -1,7 +1,8 @@
 from openai import OpenAI
 from tools import tool_registry, tools, write_report_tool_schema
-from config import FINAL_PROMPT, max_iterations, model_name
+from config import FINAL_PROMPT, max_iterations, model_name, memory_database_name
 import json
+import sqlite3
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,13 +13,86 @@ client = OpenAI()
     # перед тим як знову давати користувачу можлмивість введення, зберегти в БД в окремій таблиці самарі розмови з таким-то ід
     # і потім при наступному виклику передавати агенту самарі цієї розмови з таблиці як контекст для наступних відповідей
 
-def create_or_use_database():
-    pass
+def create_database_if_not_exist():
+    conn = sqlite3.connect(memory_database_name)
+    conn.execute("pragma foreign_keys = 1;")
+    cursor = conn.cursor()
+    cursor.execute('''
+        create table if not exists tb_sessions(
+            session_id      integer primary key autoincrement,
+            model_name      text not null,
+            summary_header  text null,
+            summary_text    text null,
+            created_at      datetime default current_timestamp
+        );
+    ''')
+    conn.commit()
+    cursor.execute('''
+        create table if not exists tb_agent_history (
+            id              integer primary key autoincrement,
+            session_id      integer not null,
+            role            text not null,
+            content         text,
+            raw_json        text,
+            prompt_tokens   integer,
+            result_tokens   integer,
+            total_tokens    integer,
+            created_at      datetime default current_timestamp,
+            foreign key (session_id) references tb_sessions (session_id) on delete cascade
+        );
+    ''')
+    conn.commit()
+    conn.close()
+
+def insert_session_database():
+    conn = sqlite3.connect(memory_database_name)
+    cursor = conn.cursor()
+    cursor.execute(
+        "insert into tb_sessions (model_name) VALUES (?)",
+        (model_name,)
+    )
+    conn.commit()
+    new_session_id = cursor.lastrowid 
+    conn.close()
+
+    return new_session_id
+
+def insert_memory_database(session_id, session_message, response_obj):
+    """
+    Inserts a single message or tool call into the tb_agent_history table.
+    
+    Args:
+        session_id (int): The ID returned from insert_session_database().
+        session_message (dict): The message dictionary (e.g., {"role": "user", "content": "..."}).
+        response_obj: The raw response object from OpenAI (used to extract tokens).
+    """
+    conn = sqlite3.connect(memory_database_name)
+    conn.execute("PRAGMA foreign_keys = ON;")
+    cursor = conn.cursor()
+    
+    role = session_message.get("role", "unknown")
+    content = session_message.get("content")
+    
+    prompt_tokens, result_tokens, total_tokens, raw_json = 0, 0, 0, ""
+    
+    # to delete
+    # print(response_obj)
+    
+    if response_obj and hasattr(response_obj, "usage") and response_obj.usage:
+        prompt_tokens = response_obj.usage.input_tokens
+        result_tokens = response_obj.usage.output_tokens
+        total_tokens = response_obj.usage.total_tokens
+        raw_json = response_obj.model_dump_json()
+        
+    cursor.execute('''
+        INSERT INTO tb_agent_history 
+        (session_id, role, content, raw_json, prompt_tokens, result_tokens, total_tokens)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (session_id, role, content, raw_json, prompt_tokens, result_tokens, total_tokens))
+    conn.commit()
+    conn.close()
 
 def get_memory_database():
-    pass
-
-def insert_memory_database():
     pass
 
 def summarize_memory_database():
@@ -46,7 +120,7 @@ def tool_execution(item):
         "output": json.dumps(result)
     }
 
-def last_call(final_prompt_text, messages):
+def last_call(final_prompt_text, messages, session_id):
     print(f"\n⚠️ Agent stopped: Reached the maximum limit of iterations. Generating final report from gathered data...")
     tools = [write_report_tool_schema]
 
@@ -59,25 +133,32 @@ def last_call(final_prompt_text, messages):
             model = model_name,
             input = messages,
             tools = tools)
-    
+
     messages_in_output = [item for item in response.output if item.type == "message"]
+
     if messages_in_output:
         final_text = messages_in_output[0].content[0].text
         print(f"\n🤖 Agent:\n{final_text}")
+        insert_memory_database(session_id, {"role": "assistant", "content": final_text}, response)
 
     messages.extend(response.output)
+
     for item in response.output:
         if item.type == "function_call":
+            insert_memory_database(session_id, {"role": "assistant", "content": f"Tool called: {item.name}"}, response)
+
             if item.name == "write_report":
                 tool_result_msg = tool_execution(item)
                 messages.append(tool_result_msg)
+                insert_memory_database(session_id, {"role": "tool", "content": tool_result_msg["output"]}, None)
                 return f"\n✅ Finished: report generated. You can continue dialog with our agent: this conversation is not forgotten!"
+
             else:
                 return f"\n🤖 Oh no... Something is not right. Please try again. Do not worry: this conversation is not forgotten, try to continue it"
     return ""
 
 
-def run_agent(messages): # debug: bool = True # додати debug mode - виводити повний текст усіх запусків, ітерації, т. д.
+def run_agent(messages, session_id): # debug: bool = True # додати debug mode - виводити повний текст усіх запусків, ітерації, т. д.
     for iteration in range(1, max_iterations + 1):
         print(f"\n🔄 Iteration {iteration} - Sending input to the model...")
 
@@ -89,6 +170,7 @@ def run_agent(messages): # debug: bool = True # додати debug mode - вив
         messages_in_output = [item for item in response.output if item.type == "message"]
         if messages_in_output:
             final_text = messages_in_output[0].content[0].text
+            insert_memory_database(session_id, {"role": "assistant", "content": final_text}, response)
             return f"\n🤖 Agent:\n{final_text}"
 
         # If no final message, append the model's tool calls to the history
@@ -96,9 +178,11 @@ def run_agent(messages): # debug: bool = True # додати debug mode - вив
         # Execute the tool calls
         for item in response.output:
             if item.type == "function_call":
+                insert_memory_database(session_id, {"role": "assistant", "content": f"Tool called: {item.name}"}, response)
                 tool_result_msg = tool_execution(item)
                 messages.append(tool_result_msg)
+                insert_memory_database(session_id, {"role": "tool", "content": tool_result_msg["output"]}, None)
 
         if iteration == max_iterations:
-            last_call(FINAL_PROMPT, messages)
+            last_call(FINAL_PROMPT, messages, session_id)
             break
--- homework-lesson-4/main.py
+++ homework-lesson-4/main.py
@@ -1,14 +1,18 @@
-from agent import tool_execution, run_agent
+from agent import tool_execution, run_agent, create_database_if_not_exist, insert_session_database, insert_memory_database
 from config import SYSTEM_PROMPT
 
 def main():
     print("Research Agent (type 'exit' to quit)")
     print("-" * 40)
 
+    create_database_if_not_exist();
+    session_id = insert_session_database()
+
     while True:
         try:
             user_input = input("\nYou: ").strip()
         except (EOFError, KeyboardInterrupt):
+            # тут зберігати самарі розмови (summary text and detail) + додати вивід по типу хвилинку, зберігаємо розмову
             print("\nGoodbye!")
             break
 
@@ -16,21 +20,28 @@ def main():
             continue
 
         if user_input.lower() in ("exit", "quit"):
+            # тут зберігати самарі розмови (summary text and detail) + додати вивід по типу хвилинку, зберігаємо розмову
             print("Goodbye!")
             break
+
+        if user_input.lower() in ("delete history"):
+            pass # тут очистити всі таблиці в БД, але зберегти структуру
 
         messages = [
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": user_input},
         ]
+        insert_memory_database(session_id, {"role": "user", "content": user_input}, 0)
 
-        agent_response = run_agent(messages)
+        agent_response = run_agent(messages,session_id)
         if agent_response:
             print(agent_response)
 
         # to delete
-        print("")
-        print(messages)
+        # print("")
+        # print(messages)
+
+        # дати юзеру опцію видалити всю історію
 
 
 if __name__ == "__main__":
