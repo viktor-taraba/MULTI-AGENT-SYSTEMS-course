05ea94a47e200ba0de042e77f4c7d92aab09a756
viktor-taraba <victortaraba@gmail.com>
2026-03-25 23:27:06 +0200

    add chunks_dir

--- homework-lesson-5/config.py
+++ homework-lesson-5/config.py
@@ -24,6 +24,7 @@ max_url_content_length: int = 5000
 embedding_model: str = "text-embedding-3-small"
 data_dir: str = "data"
 index_dir: str = "index"
+chunks_dir: str = "chunks"
 chunk_size: int = 500
 chunk_overlap: int = 100
 retrieval_top_k: int = 10
