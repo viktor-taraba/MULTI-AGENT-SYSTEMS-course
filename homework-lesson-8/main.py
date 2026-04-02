5e759b0cb4b297af3c01fbeff1ca749d57d5438f
viktor-taraba <victortaraba@gmail.com>
2026-04-02 12:36:32 +0300

    delete custom memory functions

--- homework-lesson-8/main.py
+++ homework-lesson-8/main.py
@@ -13,21 +13,9 @@ from config import SYSTEM_PROMPT, model_name_for_summary
 def main():
     print("Research Agent")
     print("type 'exit' or 'quit' to quit")
-    print("'delete history' to delete full conversation history (including previous conversations) and exit")
     print("-" * 100)
 
-    create_database_if_not_exist();
-    session_id = insert_session_database()
-    ensure_previous_session_summarized(session_id)
-
-    past_summaries = get_memory_database_summary(session_id)
-
     dynamic_system_prompt = SYSTEM_PROMPT
-    if past_summaries:
-        dynamic_system_prompt += "\n\n--- MEMORY OF PAST CONVERSATIONS ---\n"
-        for i, summary in enumerate(past_summaries, 1):
-            dynamic_system_prompt += f"Session {i}: {summary}\n"
-
     messages = [
         {"role": "system", "content": dynamic_system_prompt}
     ]
@@ -50,11 +38,6 @@ def main():
             print("Goodbye!")
             break
 
-        if user_input.lower() in ("delete history"):
-            print(truncate_database())
-            break
-
-        insert_memory_database(session_id, {"role": "user", "content": user_input}, 0)
         messages.append({"role": "user", "content": user_input})
 
         agent_response = run_agent(messages,session_id)
