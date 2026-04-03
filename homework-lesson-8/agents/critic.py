9d055d22000a2db5be17d0f327bfcc5f49542470
viktor-taraba <victortaraba@gmail.com>
2026-04-04 02:40:08 +0300

    formatting

--- homework-lesson-8/agents/critic.py
+++ homework-lesson-8/agents/critic.py
@@ -1,12 +1,22 @@
 from langchain.agents import create_agent
 
 # to delete
-import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#import os, sys
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from schemas import CritiqueResult
-from config import critic_model_name, SYSTEM_PROMPT_critic, max_iterations_critic
-from tools import web_search, read_url, knowledge_search, stock_company_info, find_articles_crossref
+from config import (
+    critic_model_name, 
+    SYSTEM_PROMPT_critic, 
+    max_iterations_critic
+)
+from tools import (
+    web_search, 
+    read_url, 
+    knowledge_search, 
+    stock_company_info, 
+    find_articles_crossref
+)
 
 critic_agent = create_agent(
     model=critic_model_name,
--- homework-lesson-8/agents/planner.py
+++ homework-lesson-8/agents/planner.py
@@ -1,12 +1,18 @@
 from langchain.agents import create_agent
 
 # to delete
-import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#import os, sys
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from schemas import ResearchPlan
-from config import planner_model_name, SYSTEM_PROMPT_planner
-from tools import web_search, knowledge_search
+from config import (
+    planner_model_name, 
+    SYSTEM_PROMPT_planner
+)
+from tools import (
+    web_search, 
+    knowledge_search
+)
 
 planner_agent = create_agent(
     model=planner_model_name,
