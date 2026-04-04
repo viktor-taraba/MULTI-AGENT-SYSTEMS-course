39108e5d6217be53c1be30d67ccea6d4e55cccaf
viktor-taraba <victortaraba@gmail.com>
2026-04-04 18:16:41 +0300

    add JsonPlusSerializer

--- homework-lesson-8/agents/critic.py
+++ homework-lesson-8/agents/critic.py
@@ -18,11 +18,17 @@ from tools import (
     find_articles_crossref
 )
 from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.serde.jsonplus import JsonPlusSerializer
+
+custom_serializer = JsonPlusSerializer(
+    allowed_msgpack_modules=[('schemas', 'CritiqueResult')]
+)
+memory = InMemorySaver(serde=custom_serializer)
 
 critic_agent = create_agent(
     model=critic_model_name,
     tools=[web_search, read_url, knowledge_search, stock_company_info, find_articles_crossref],
     system_prompt=SYSTEM_PROMPT_critic,
     response_format=CritiqueResult,
-    checkpointer=InMemorySaver()
+    checkpointer=memory
 )
--- homework-lesson-8/agents/planner.py
+++ homework-lesson-8/agents/planner.py
@@ -9,11 +9,17 @@ from tools import (
     knowledge_search
 )
 from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.serde.jsonplus import JsonPlusSerializer
+
+custom_serializer = JsonPlusSerializer(
+    allowed_msgpack_modules=[('schemas', 'ResearchPlan')]
+)
+memory = InMemorySaver(serde=custom_serializer)
 
 planner_agent = create_agent(
     model=planner_model_name,
     tools=[web_search, knowledge_search],
     system_prompt=SYSTEM_PROMPT_planner,
     response_format=ResearchPlan,
-    checkpointer=InMemorySaver()
+    checkpointer=memory
 )
