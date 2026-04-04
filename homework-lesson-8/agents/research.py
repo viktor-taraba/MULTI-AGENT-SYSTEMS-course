5672897ff36958a1280e43260f11e1b810cefc6b
viktor-taraba <victortaraba@gmail.com>
2026-04-04 17:30:12 +0300

    delete max_iterations_research param

--- homework-lesson-8/agents/research.py
+++ homework-lesson-8/agents/research.py
@@ -1,13 +1,8 @@
 from langchain.agents import create_agent
 
-# to delete
-#import os, sys
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from config import (
     research_model_name, 
-    SYSTEM_PROMPT_research, 
-    max_iterations_research
+    SYSTEM_PROMPT_research
 )
 from schemas import ResearchResult
 from tools import (
@@ -33,20 +28,3 @@ research_agent = create_agent(
     response_format=ResearchResult,
     checkpointer=memory
 )
-
-"""
-#config = {"recursion_limit": max_iterations_research, "early_stopping_method": "generate"}
-
-# Test Planner Agent in isolation
-for step in research_agent.stream({
-    "messages": [
-        {
-            "role": "user",
-            "content": "pbir files validation"
-        }
-    ]
-}):#, config = config):
-    for update in step.values():
-        for message in update.get("messages", []):
-            message.pretty_print()
-"""
