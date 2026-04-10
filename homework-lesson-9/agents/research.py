ee1a8f699000118e001e185f14c18a8c4f2b78e1
viktor-taraba <victortaraba@gmail.com>
2026-04-11 00:32:25 +0300

    run_research with search_mcp tools

--- homework-lesson-9/agents/research.py
+++ homework-lesson-9/agents/research.py
@@ -2,16 +2,14 @@ from langchain.agents import create_agent
 from config import (
     research_model_name, 
     SYSTEM_PROMPT_research,
-    ToolCallLimit_research
+    ToolCallLimit_research,
+    port_search_mcp
 )
 from schemas import ResearchResult
-from tools import (
-    web_search, 
-    read_url, 
-    knowledge_search, 
-    stock_company_info, 
-    find_articles_crossref
-)
+from fastmcp import Client
+from mcp_utils import mcp_tools_to_langchain
+from supervisor import print_agent_step
+
 from langchain.agents.middleware.tool_call_limit import ToolCallLimitMiddleware
 
 tool_limiter = ToolCallLimitMiddleware(
@@ -19,10 +17,28 @@ tool_limiter = ToolCallLimitMiddleware(
     exit_behavior="error"
 )
 
-research_agent = create_agent(
-    model=research_model_name,
-    tools=[web_search, read_url, knowledge_search, stock_company_info, find_articles_crossref],
-    system_prompt=SYSTEM_PROMPT_research,
-    response_format=ResearchResult,
-    middleware=[tool_limiter]
-)
+port_search = f"http://127.0.0.1:{port_search_mcp}/mcp"
+
+async def run_research(user_text: str) -> str:
+    async with Client(port_search) as mcp_client:
+        
+        # Fetch and convert tools
+        mcp_tools = await mcp_client.list_tools()
+        lc_tools = mcp_tools_to_langchain(mcp_tools, mcp_client)
+
+        # Create the agent with the dynamically fetched tools
+        research_agent = create_agent(
+            model=research_model_name,
+            tools=lc_tools, 
+            system_prompt=SYSTEM_PROMPT_research,
+            response_format=ResearchResult,
+            middleware=[tool_limiter]
+        )
+
+        # Run the agent while the connection is still open
+        result = await research_agent.ainvoke({"messages": [("user", user_text)]})
+             
+        for msg in result["messages"][1:]:
+            print_agent_step(msg, agent_name="Planner")
+
+        return result["messages"][-1].content
