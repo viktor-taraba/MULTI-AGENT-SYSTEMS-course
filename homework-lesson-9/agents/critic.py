1c2b58d3630e13841a6b5ad3b6cab79c16fb8ff8
viktor-taraba <victortaraba@gmail.com>
2026-04-11 00:32:08 +0300

    run_critic with search_mcp tools

--- homework-lesson-9/agents/critic.py
+++ homework-lesson-9/agents/critic.py
@@ -2,19 +2,31 @@ from langchain.agents import create_agent
 from schemas import CritiqueResult
 from config import (
     critic_model_name, 
-    SYSTEM_PROMPT_critic
+    SYSTEM_PROMPT_critic,
+    port_search_mcp
 )
-from tools import (
-    web_search, 
-    read_url, 
-    knowledge_search, 
-    stock_company_info, 
-    find_articles_crossref
-)
+from schemas import ResearchResult
+from fastmcp import Client
+from mcp_utils import mcp_tools_to_langchain
+from supervisor import print_agent_step
 
-critic_agent = create_agent(
-    model=critic_model_name,
-    tools=[web_search, read_url, knowledge_search, stock_company_info, find_articles_crossref],
-    system_prompt=SYSTEM_PROMPT_critic,
-    response_format=CritiqueResult
-)
+port_search = f"http://127.0.0.1:{port_search_mcp}/mcp"
+
+async def run_critic(user_text: str) -> str:
+    async with Client(port_search) as mcp_client:
+        
+        mcp_tools = await mcp_client.list_tools()
+        lc_tools = mcp_tools_to_langchain(mcp_tools, mcp_client)
+
+        critic_agent = create_agent(
+            model=critic_model_name,
+            tools=lc_tools, 
+            system_prompt=SYSTEM_PROMPT_critic,
+            response_format=CritiqueResult
+        )
+
+        result = await critic_agent.ainvoke({"messages": [("user", user_text)]})
+
+        for msg in result["messages"][1:]:
+            print_agent_step(msg, agent_name="Planner")
+        return result["messages"][-1].content
