33d8111c10b263161830b74910886052f47848d2
viktor-taraba <victortaraba@gmail.com>
2026-04-12 20:47:37 +0300

    delete testing prints

--- homework-lesson-9/acp_server.py
+++ homework-lesson-9/acp_server.py
@@ -38,60 +38,6 @@ async def critic_handler(input: list[Message]) -> Message:
 def run_acp():
     acp_server.run(port=port_acp_server)
 
-"""
-async def demo_acp():
-    async with ACPClient(base_url=acp_adress, headers={"Content-Type": "application/json"}) as client:
-        # Discovery
-        agents = [a async for a in client.agents()]
-        print("ACP Discovery:")
-        for a in agents:
-            print(f"   {a.name}: {a.description}")
-        print()
-
-        # Call planner
-        print('-' * 50)
-        print("Calling 'planner'...")
-        run = await client.run_sync(
-            agent="planner",
-            input=[Message(role="user", parts=[MessagePart(content="What is BM25 algorithm and how is it used in search engines?")])],
-        )
-        output = run.output[-1].parts[0].content
-        print(f"**Result:**\n{output}")
-        print()
-        print('-' * 50)
-        print()
-
-        # Call researcher
-        print('-' * 50)
-        print("Calling 'researcher'...")
-        run = await client.run_sync(
-            agent="researcher",
-            input=[Message(role="user", parts=[MessagePart(content="Agentic development in power bi. Use only one tool 'knowledge search' several times")])],
-        )
-        output = run.output[-1].parts[0].content
-        print(f"**Result:**\n{output}")
-        print()
-        print('-' * 50)
-
-        # Call critic
-        print('-' * 50)
-        print("Calling 'critic'...")
-        run = await client.run_sync(
-            agent="critic",
-            input=[Message(role="user", parts=[MessagePart(content="Test this report")])],
-        )
-        output = run.output[-1].parts[0].content
-        print(f"**Result:**\n{output}")
-        print()
-        print('-' * 50)
-
-if __name__ == "__main__":
-    threading.Thread(target=run_acp, daemon=True).start()
-    time.sleep(2)
-    print(f"✅ ACP Server running at http://127.0.0.1:{acp_adress}")
-    asyncio.run(demo_acp())
-]"""
-
 if __name__ == "__main__":
     print(f"✅ Starting ACP Server on {acp_adress}")
     try:
