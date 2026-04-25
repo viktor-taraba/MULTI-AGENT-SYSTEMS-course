4cdf2d3e925c7e6f097d46dabf59b86fe097ffd2
viktor-taraba <victortaraba@gmail.com>
2026-04-26 00:04:52 +0300

    add test_critique_approve

--- homework-lesson-10/tests/test_critic.py
+++ homework-lesson-10/tests/test_critic.py
@@ -1,8 +1,7 @@
 from deepeval.metrics import GEval
 from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 from agents.critic import critic_agent
-from helper import evaluate_and_assert, extract_output_and_context
-import json 
+from helper import evaluate_and_assert
 
 critique_quality = GEval(
     name="Critique Quality",
@@ -17,13 +16,23 @@ critique_quality = GEval(
     threshold=0.7,
 )
 
-# тут хардом прописати для критика якийсь поганий звіт
-# і взяти з прикладів в аутпуті хороший 
-
 def test_critique_approve():
     with open("tests/critic_tests_examples/pbir_multi_agent_prompting_report.md", "r", encoding="utf-8") as f:
         approve_report = f.read()
-    pass
+
+    user_input = f"Review report: {approve_report}"
+    agent_response = critic_agent.invoke(
+            {"messages": [("user", user_input)]}, 
+            config={"configurable": {"thread_id": "test_thread_001"}}
+        )
+    actual_output_str = str(agent_response.get("messages", [])[-1].content)
+        
+    test_case = LLMTestCase(
+        input=user_input,
+        actual_output=actual_output_str
+    )
+    critique_quality.measure(test_case)
+    evaluate_and_assert(critique_quality, "test_critique_approve", "critique_quality")
 
 def test_critique_revise():
     with open("tests/critic_tests_examples/ponziani_opening_report.md", "r", encoding="utf-8") as f:
@@ -34,10 +43,7 @@ def test_critique_revise():
             {"messages": [("user", user_input)]}, 
             config={"configurable": {"thread_id": "test_thread_002"}}
         )
-
-    # to delete
     actual_output_str = str(agent_response.get("messages", [])[-1].content)
-    print(actual_output_str)
         
     test_case = LLMTestCase(
         input=user_input,
