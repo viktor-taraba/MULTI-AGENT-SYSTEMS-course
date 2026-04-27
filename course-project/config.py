5415eeb60c25e04db881173e4868eb5e87d8a38a
viktor-taraba <victortaraba@gmail.com>
2026-04-27 20:23:14 +0300

    add database and server

---
+++ course-project/config.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from langfuse import get_client
+from dotenv import load_dotenv
+load_dotenv()
+
+# for prompts
+langfuse_prompts = get_client()
+
+# critic agent
+revision_counter_max: int = 1
+critic_model_name: str = "gpt-5-mini"
+max_iterations_critic: int = 25
+max_items_critic: int = 5
+SYSTEM_PROMPT_critic = langfuse_prompts.get_prompt("SYSTEM_PROMPT_critic", label="production").compile(
+                        current_date=datetime.now().isoformat(),
+                        max_items_critic=max_items_critic)
+FINAL_PROMPT_critic = langfuse_prompts.get_prompt("FINAL_PROMPT_critic", label="production").compile()
+
+# research agent
+research_model_name: str = "gpt-5-mini"
+max_iterations_research: int = 80
+ToolCallLimit_research: int = 25
+SYSTEM_PROMPT_research = langfuse_prompts.get_prompt("SYSTEM_PROMPT_research", label="production").compile()
+FINAL_PROMPT_research = langfuse_prompts.get_prompt("FINAL_PROMPT_research", label="production").compile()
+
+# planner agent
+planner_model_name: str = "gpt-5-mini"
+max_iterations_planner: int = 30
+SYSTEM_PROMPT_planner = langfuse_prompts.get_prompt("SYSTEM_PROMPT_planner", label="production").compile()
+FINAL_PROMPT_planner = langfuse_prompts.get_prompt("FINAL_PROMPT_planner", label="production").compile()
+
+# tools 
+max_search_results: int = 5
+max_url_content_length: int = 5000
+tool_preview_len: int = 100
+server: str = "(localdb)\\MSSQLLocalDB"
+database: str = "AdventureWorks2022"
+
+# RAG
+embedding_model: str = "text-embedding-3-small"
+cross_encoder_model: str = "BAAI/bge-reranker-base"
+data_dir: str = "data"
+index_dir: str = "index"
+collection_name: str = "documents_collection"
+chunks_dir: str = "chunks"
+chunks_json_name: str = "bm25_chunks.json"
+chunk_size: int = 500
+chunk_overlap: int = 100
+retrieval_top_k: int = 10
+rerank_top_n: int = 3
+BM25_retriever_weight: float = 0.4
+vector_retriever_weight: float = 1 - BM25_retriever_weight
+Youtube_links_file_name = "Youtube_links.txt"
