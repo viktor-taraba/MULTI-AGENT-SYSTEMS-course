1987651dc973ec3d17622481ee1b55c53850fca3
viktor-taraba <victortaraba@gmail.com>
2026-05-01 00:22:29 +0300

    create reviewer.py (QA agent)

---
+++ course-project/agents/reviewer.py
@@ -0,0 +1,27 @@
+from langchain.agents import create_agent
+from schemas import ReviewOutput
+from config import (
+    LLM_POWERFUL, 
+    Reviewer_SYSTEM_prompt
+)
+from tools import (
+    knowledge_search,
+    get_table_structure,
+    execute_sql_query
+)
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.serde.jsonplus import JsonPlusSerializer
+
+custom_serializer = JsonPlusSerializer(
+    allowed_msgpack_modules=[('schemas', 'ReviewOutput')]
+)
+memory = InMemorySaver(serde=custom_serializer)
+
+reviewer = create_agent(
+    model=LLM_POWERFUL,
+    tools=[knowledge_search,get_table_structure,execute_sql_query],
+    system_prompt=Reviewer_SYSTEM_prompt,
+    response_format=ReviewOutput,
+    checkpointer=memory,
+    name="reviewer",
+)
