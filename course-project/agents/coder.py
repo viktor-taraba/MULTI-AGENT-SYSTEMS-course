b5a02fa3b8d4174de46250a8fc02cb9ca6e0bd9f
viktor-taraba <victortaraba@gmail.com>
2026-04-30 23:48:57 +0300

    create coder_.py (developer agent)

---
+++ course-project/agents/coder.py
@@ -0,0 +1,29 @@
+from langchain.agents import create_agent
+from schemas import CodeOutput
+from config import (
+    LLM_FAST, 
+    Coder_SYSTEM_prompt
+)
+from tools import (
+    web_search, 
+    knowledge_search,
+    read_url,
+    get_table_structure,
+    execute_sql_query
+)
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.serde.jsonplus import JsonPlusSerializer
+
+custom_serializer = JsonPlusSerializer(
+    allowed_msgpack_modules=[('schemas', 'CodeOutput')]
+)
+memory = InMemorySaver(serde=custom_serializer)
+
+coder = create_agent(
+    model=LLM_FAST,
+    tools=[knowledge_search,web_search,read_url,get_table_structure,execute_sql_query],
+    system_prompt=Coder_SYSTEM_prompt,
+    response_format=CodeOutput,
+    checkpointer=memory,
+    name="coder",
+)
