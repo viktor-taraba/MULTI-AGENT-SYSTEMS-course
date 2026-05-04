b660ffbe5db022741649efb918de3f6a24e7d800
viktor-taraba <victortaraba@gmail.com>
2026-05-04 16:43:12 +0300

    add tool get_view_definition

--- course-project/agents/planner.py
+++ course-project/agents/planner.py
@@ -10,7 +10,8 @@ from tools import (
     read_url,
     ask_user_for_clarification,
     list_schemas_and_tables,
-    get_sample_rows
+    get_sample_rows,
+    get_view_definition
 )
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.checkpoint.serde.jsonplus import JsonPlusSerializer
@@ -22,7 +23,7 @@ memory = InMemorySaver(serde=custom_serializer)
 
 planner = create_agent(
     model=LLM_POWERFUL,
-    tools=[knowledge_search,web_search,read_url,ask_user_for_clarification,list_schemas_and_tables,get_sample_rows],
+    tools=[knowledge_search,web_search,read_url,ask_user_for_clarification,list_schemas_and_tables,get_sample_rows,get_view_definition],
     system_prompt=Business_Analyst_SYSTEM_prompt,
     response_format=SpecOutput,
     checkpointer=memory,
